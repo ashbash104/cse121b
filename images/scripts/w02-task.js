5e0b97e19177f3c9a3df244e9defffba7946705b
Ashlee Hart <ashlee.hart104@gmail.com>
2023-09-29 14:59:31 -0600

    Made progress on week 02 file

--- images/scripts/w02-task.js
+++ images/scripts/w02-task.js
@@ -8,16 +8,19 @@ let fullName = 'Ashlee Hart';
 let currentYear = '2023';
 let profilePicture = 'images/me.JPG';
 
-
 /* Step 3 - Element Variables */
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const image = new Image();
+image.src = "images/me.JPG";
 
 
 
 
 /* Step 4 - Adding Content */
-
-
+nameElement.innerHTML = '<strong>${fullName}</strong>';
+// Left off on number 4 of part 4.
 
 
 
